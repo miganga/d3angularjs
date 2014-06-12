a3f271a11e296ba8cfdd53eeac79a473c1c626c5
Christian Rösike <cr@itembase.biz>
2014-06-12 16:22:57 +0200

    added new styling to the project

--- app/scripts/directives/linechart.js
+++ app/scripts/directives/linechart.js
@@ -55,13 +55,13 @@ angular.module('partnerApp')
                     // assign the X function to plot our line as we wish
                     .x(function(d,i) {
                         // verbose logging to show what's actually being done
-                        console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+                        //console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
                         // return the X coordinate where we want to plot this datapoint
                         return x(i);
                     })
                     .y(function(d) {
                         // verbose logging to show what's actually being done
-                        console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+                        //console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
                         // return the Y coordinate where we want to plot this datapoint
                         return y(d);
                     });
